9d3c9b4afe491000518c39724482f6a375446972
Ben Fu <benruoheng@gmail.com>
2022-11-01 20:33:52 -0400

    added a on_ready function

--- bot.py
+++ bot.py
@@ -5,4 +5,7 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print('We have logged in as {0.users}'.format(client))
+#1036439896354725950
+    general_channel = client.get_channel(1036439896354725950)
+    await general_channel.send("Hello")
 client.run(TOKEN)
