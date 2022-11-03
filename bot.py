318907a05e2941b9ca43ac9d50a7d58c958031ea
Ben Fu <benruoheng@gmail.com>
2022-11-02 21:48:37 -0400

    on_message

--- bot.py
+++ bot.py
@@ -1,16 +1,23 @@
 import discord 
 import random
-TOKEN = ''
-intents = discord.Intents.default()
+TOKEN = 'MTAzNjg2MjE4MjIwMzMyNjUzNQ.GPjAuZ.4f2m2uEOnuw_fQbxJW0ZFUuABlhTca8NiSJkg8'
+intents = discord.Intents().all()
 client = discord.Client(intents=intents)
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.users}'.format(client))
-#1036439896354725950
-    #general_channel = client.get_channel(1036439896354725950)
-    #await general_channel.send("Hello")
-@bot.event
-async def on_member_join(member):
-    channel = bot.get_channel('1036439896354725950')
-    await channel.send("Hello {user}, Welcome to the server!")
+
+@client.event 
+async def on_message(message):
+    id = client.get_guild(1036439895905939546)
+    if message.author == client .user:
+        return 
+
+    if message.content.startswith('!hi'):
+        await message.channel.send('hi')
+    
+    elif message.content == '!users':
+        await message.channel.send(f""" Number of Members {id.member_count} """)
+
 client.run(TOKEN)
