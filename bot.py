09109830f75afb4f0fe7761adf19f0a17bfd670f
Ben Fu <benruoheng@gmail.com>
2022-11-04 00:52:08 -0400

    welcome members

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 import discord 
-import random
-from discord.ext import commands
-TOKEN = ''
+TOKEN = 'MTAzNjg2MjE4MjIwMzMyNjUzNQ.G7_hyk.ZvXwLIJ8sErOg3rQu2ixCfyPj4umhUDxWwCbbY'
 intents = discord.Intents().all()
+intents.members = True
 client = discord.Client(intents=intents)
+
 
 @client.event
 async def on_ready():
@@ -19,11 +19,18 @@ async def on_message(message):
         await message.channel.send('hi')
     
     elif message.content == '!users':
-        await message.channel.send(f" Number of Members {id.member_count} ")
+        await message.channel.send(f'Number of Members: {id.member_count}')
     
     if message.content.startswith('!help'):
         help_command = discord.Embed(colour=0x00ff00)
         help_command.add_field(name = 'Commands:', value = '!important dates\n!TA hours\n!Due dates', inline=False)
         await message.channel.send(embed=help_command)
+
+@client.event
+async def on_member_join(member):
+    id = client.get_guild(1036439895905939546)
+    channel = client.get_channel(1036439896354725950)
+    await channel.send(f'Welcome to the server {member.mention}! ' + ':tada:'*3)
+    await member.send(f'Welcome to {id.name}, {member.name}!')
 
 client.run(TOKEN)
